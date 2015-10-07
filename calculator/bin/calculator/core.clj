a3d50e8d0cd0dcc6aacdfd5fec4eafad807db534
Will DeVries <princetyke@hotmail.com>
2015-10-07 19:58:27 -0400

    Made buttons, they populate the text box

--- calculator/bin/calculator/core.clj
+++ calculator/bin/calculator/core.clj
@@ -1,8 +1,113 @@
-(ns calculator.core)
+(ns calculator.core (:gen-class))
 
 (use 'seesaw.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def mainframe (frame :title "Clojure Calculator"
+                      :size [230 :by 250]
+                      :resizable? false
+                      :on-close :exit))
+
+(defn display [c]
+  (config! mainframe :content c)
+  c)
+
+(def t (text :editable? false))
+
+(def one (button :text "1"))
+(listen one 
+        :mouse-clicked (fn [e] (config! t :text (str (text t) "1"))))
+
+(def two (button :text "2"))
+(listen two 
+        :mouse-clicked (fn [e] (config! t :text (str (text t) "2"))))
+
+(def three (button :text "3"))
+(listen three 
+        :mouse-clicked (fn [e] (config! t :text (str (text t) "3"))))
+
+(def four (button :text "4"))
+(listen four 
+        :mouse-clicked (fn [e] (config! t :text (str (text t) "4"))))
+
+(def five (button :text "5"))
+(listen five 
+        :mouse-clicked (fn [e] (config! t :text (str (text t) "5"))))
+
+(def six (button :text "6"))
+(listen six 
+        :mouse-clicked (fn [e] (config! t :text (str (text t) "6"))))
+
+(def sev (button :text "7"))
+(listen sev 
+        :mouse-clicked (fn [e] (config! t :text (str (text t) "7"))))
+
+(def eight (button :text "8"))
+(listen eight 
+        :mouse-clicked (fn [e] (config! t :text (str (text t) "8"))))
+
+(def nine (button :text "9"))
+(listen nine 
+        :mouse-clicked (fn [e] (config! t :text (str (text t) "9"))))
+
+(def zero (button :text "0"))
+(listen zero 
+        :mouse-clicked (fn [e] (config! t :text (str (text t) "0"))))
+
+(def dot (button :text "."))
+(listen dot 
+        :mouse-clicked (fn [e] (config! t :text (str (text t) "."))))
+
+(def cl (button :text "Clear"))
+(listen cl
+        :mouse-clicked (fn [e] (config! t :text "")))
+
+(def bp (border-panel
+          :center
+          (flow-panel
+            :items 
+               [one
+                two
+                three
+                four
+                five
+                six
+                sev
+                eight
+                nine 
+                zero
+                dot])
+          :north t
+          :south "SOUTH"
+          :east cl
+          :hgap 10
+          :vgap 10))
+
+(def gp (grid-panel
+          :columns 1
+          :items
+          [(grid-panel
+             :columns 3
+             :items
+             [(button :text "1" :bounds [:* :* 50 50]) 
+              (button :text "2")
+              (button :text "3")
+              (button :text "4")
+              (button :text "5")
+              (button :text "6")
+              (button :text "7")
+              (button :text "8")
+              (button :text "9")]
+             :vgap 5
+             :hgap 5)
+           (grid-panel
+             :columns 2
+             :items 
+             [(button :text "0")
+              (button :text ".")]
+             :vgap 5
+             :hgap 5)]))
+
+(defn -main [& args]
+  (config! mainframe :content bp)
+  (-> mainframe show!)
+  )
