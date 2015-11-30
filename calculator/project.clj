1ddc6b72a2e8babc3728834abc2598211843ac5c
Mark <mdwoodfo@mtu.edu>
2015-11-30 18:56:39 -0500

    Defined arithmetic parser
    I got this from https://github.com/Engelberg/instaparse.

--- calculator/project.clj
+++ calculator/project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [seesaw "1.4.2" :exclusions [org.clojure/clojure]]]
+                 [seesaw "1.4.2" :exclusions [org.clojure/clojure]]
+                 [instaparse "1.4.1"]]
   :main calculator.core)
---
+++ calculator/src/calculator/parser.clj
@@ -0,0 +1,28 @@
+(ns calculator.parser
+  (:require [instaparse.core :as insta]))  ;; "instaparse" is a parser that takes a BNF grammar
+
+
+;; Adapted from https://github.com/Engelberg/instaparse
+(def arithmetic
+  (insta/parser
+    "expr = add-sub
+     <add-sub> = mul-div | add | sub
+     add = add-sub <'+'> mul-div
+     sub = add-sub <'-'> mul-div
+     <mul-div> = term | mul | div
+     mul = mul-div <'*'> term
+     div = mul-div <'/'> term
+     <term> = number | <'('> add-sub <')'>
+     number = #'[0-9]+'"))
+
+(def transform-options
+  {:number read-string,
+   :add +,
+   :sub -,
+   :mul *,
+   :div /,
+   :expr identity})
+
+(defn parse [input]
+   (->> (arithmetic input)
+        (insta/transform transform-options)))
