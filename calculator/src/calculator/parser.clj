4314ac1cffdeab8378636483fdf35e2227a8e42a
Mark <mdwoodfo@mtu.edu>
2015-12-01 21:50:26 -0500

    Big Decimal representation
    Improve accuracy of calculations by representing the numbers with Big
    Decimal. Limit precision to 32 digits so that everything fits in the
    display.

--- calculator/src/calculator/parser.clj
+++ calculator/src/calculator/parser.clj
@@ -12,17 +12,42 @@
      <mul-div> = term | mul | div
      mul = mul-div <'*'> term
      div = mul-div <'/'> term
-     <term> = number | <'('> add-sub <')'>
-     number = #'[0-9]+'"
+     <term> = num | <'('> add-sub <')'>
+     num = negative? (decimal-num | integer-num)
+     <negative> = '-'
+     <decimal-num> = digit* '.' digit+
+     <digit> = #'[0-9]'
+     <integer-num> = digit+"
     :auto-whitespace :standard))
+
+(def precision 32)
+
+(defn parse-num [& tokens]
+  (bigdec (apply str tokens)))
+
+(defn addition [& nums]
+  (with-precision precision
+    (apply + nums)))
+
+(defn subtraction [& nums]
+  (with-precision precision
+    (apply - nums)))
+
+(defn multiplication [& nums]
+  (with-precision precision
+    (apply * nums)))
+
+(defn division [& nums]
+  (with-precision precision
+    (apply / nums)))
 
 ;; Defines what Clojure functions to replace parsed tokens with
 (def transform-options
-  {:number read-string,
-   :add +,
-   :sub -,
-   :mul *,
-   :div /,
+  {:num parse-num,
+   :add addition,
+   :sub subtraction,
+   :mul multiplication,
+   :div division,
    :expr identity})
 
 ;; Parses the input string and transforms it into a Clojure expression
@@ -30,5 +55,10 @@
 (defn parse [input]
   (let [result (arithmetic input)]
     (if (insta/failure? result)
-      ""
-      (insta/transform transform-options result))))
+      (do (println (str "ERR Parse\n" result))
+          "ERR")
+      (try
+        (str (insta/transform transform-options result))
+        (catch Exception e
+          (do (println (str "ERR Transform\n" (.getMessage e)))
+              "ERR"))))))
